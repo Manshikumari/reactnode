0c0ac3ac89dc84fe29c76aa18bbb256937e6f261
Manshikumari <88151072+Manshikumari@users.noreply.github.com>
2023-12-21 16:55:37 +0000

    pending

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// // // rfce
+// // import React from 'react'
+
+// // function App() {
+
+// //   let data={
+// //     background:"green",
+// //     fontSize:"40px"
+// //   }
+// //   let data2={
+// //     color:"white",
+// //     background:"blue"
+// //   }
+// //   return (
+// //     <div>
+// //       <h2>App Components</h2>
+// //       <p style={{...data,...data2}}>i m here now </p>
+// //     </div>
+// //   )
+// // }
+
+// // export default App
+// import React,{useState}from 'react'
+
+// function App() {
+
+//   let[val,setval]=useState(true)
+//  let data={
+//     background:"yellow",
+//   }
+//    let data2={
+//     background:"red",
+//   }
+//   let show=()=>{
+//     // setval(false)
+//     setval(val?false:true)
+//   }
+//   return (
+//     <div>
+//        <h2 style={val?data:data2}>i m here now</h2> 
+//        <button onClick={show}>click now</button>
+         
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
 
 function App() {
+   show=()=>{
+    console.log("button click")
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. i m here now
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={this.show}>click here now</button>
     </div>
-  );
+  )
 }
 
-
-export default App;
+export default App
