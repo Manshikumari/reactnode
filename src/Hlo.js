cd25e6a1f9ddea618deec6cb74fa14ac28876329
Manshikumari <88151072+Manshikumari@users.noreply.github.com>
2024-02-05 06:01:54 +0000

    pending

--- src/Hlo.js
+++ src/Hlo.js
@@ -2,64 +2,19 @@ import React from 'react'
 
 function Hlo() {
     return (
-        <div>
-            <div className='main'>
-                <div className='left'>
-                    <div className="container">
-                        <h1 className="title">Sign Up</h1>
-                        <form onSubmit={handleSubmit} className="form">
-                            <label className="label">
-                                Username:
-                                <input
-                                    type="text"
-                                    name="username"
-                                    value={formData.username}
-                                    onChange={handleChange}
-                                    className="input"
-                                />
-                            </label>
-                            <br />
-
-                            <label className="label">
-                                Email:
-                                <input
-                                    type="email"
-                                    name="email"
-                                    value={formData.email}
-                                    onChange={handleChange}
-                                    className="input"
-                                />
-                            </label>
-                            <br />
-
-                            <label className="label">
-                                Password:
-                                <input
-                                    type="password"
-                                    name="password"
-                                    value={formData.password}
-                                    onChange={handleChange}
-                                    className="input"
-                                />
-                            </label>
-                            <br />
-
-                            <button type="submit" className="button">
-                                Sign Up
-                            </button>
-                        </form>
-                    </div>
-                </div>
-                <div className='right'>
-
-                </div>
+      <div className='main'>
+            <div className='head'>
+                <h2 id='head'> Sign Up </h2>
             </div>
-
-            );
-};
+            <div className='body'>
+            <form>
+               <label id='pno'> Phone No. </label>
+              <input type='text' id='box'></input>
+            </form>
+        </div>
         </div>
     )
-}
+};
 
 export default Hlo
 
